efd80c6c79cb6e6a72e1de1bc2e9500097806bb3
tanvir <tanvir101tk@gmail.com>
2022-10-11 00:33:20 +0600

    all component create

---
+++ src/component/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            this is home
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/component/Statics/Statics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Statics.css'
+const Statics = () => {
+    return (
+        <div>
+            this is statics
+        </div>
+    );
+};
+
+export default Statics;
