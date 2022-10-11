07e566e109cde6e88d2d51780c3babfdedf6db3b
tanvir <tanvir101tk@gmail.com>
2022-10-12 01:31:28 +0600

    topics page done

--- src/Statics/Statics.js
+++ src/Statics/Statics.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { LoadedData } from '../component/Layout';
 import './Statics.css'
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }];
 const Statics = () => {
+    const data = useContext(LoadedData)
+    data.map(d => (d.total))
     return (
         <div>
-            this is statics
+            <BarChart width={600} height={300} data={data}>
+                <XAxis dataKey="name" stroke="#8884d8" />
+                <YAxis />
+                <Tooltip />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <Bar dataKey="uv" fill="#8884d8" barSize={30} />
+            </BarChart>
         </div>
     );
 };
