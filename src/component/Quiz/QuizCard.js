da755d42678569b19eed09fb67960a89f52e5d71
tanvir <tanvir101tk@gmail.com>
2022-10-12 22:59:39 +0600

    toast added

--- src/component/Quiz/QuizCard.js
+++ src/component/Quiz/QuizCard.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './QuizCart.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const QuizCard = ({ ques }) => {
     const { question, options, correctAnswer } = ques;
+
+    const handleToast = (e) => {
+        if (e === correctAnswer) {
+            alert('Your answer is correct')
+        }
+        else {
+            alert('Wrong answer')
+        }
+    }
     return (
         <div className='question container'>
             <h4>{question}</h4>
-            {options.map(single => <p key={single}>{single}</p>)}
+            {options.map(single => <p onClick={(e) => handleToast(e.target.innerText)} key={single}>{single}</p>)}
         </div>
     );
 };
